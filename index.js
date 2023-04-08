9d6acf7cf68f35cfe313e7efed79cfac92cea0c0
javron <towersjavin@gmail.com>
2023-04-08 15:29:10 -0700

    Update Vercel configuration for serverless functions

--- index.js
+++ index.js
@@ -1,55 +1,11 @@
 const express = require('express');
-const axios = require('axios');
-const cheerio = require('cheerio');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const path = require('path');
-const fs = require('fs');
+app.use(express.static(path.join(__dirname)));
 
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// ... (previous imports and setup)
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-  });
-  
-app.post('/scrape', async (req, res) => {
-    const url = req.body.url;
-    if (!url) {
-      return res.status(400).send({ error: 'Please provide a URL.' });
-    }
-  
-    try {
-      const response = await axios.get(url);
-      const $ = cheerio.load(response.data);
-  
-      const html = $('body').html();
-      const usedClasses = new Set();
-      $('[class]').each((_, element) => {
-        const classes = $(element).attr('class').split(' ');
-        classes.forEach((className) => usedClasses.add(className));
-      });
-  
-      const cssPromises = [];
-      $('link[rel="stylesheet"]').each((_, link) => {
-        const href = $(link).attr('href');
-        cssPromises.push(axios.get(href));
-      });
-  
-      const cssResponses = await Promise.all(cssPromises);
-      const cssData = cssResponses.map((response) => response.data).join('\n');
-      const usedCss = extractUsedCss(cssData, usedClasses);
-  
-      res.set('Content-Type', 'text/plain');
-      res.send({ html, css: usedCss });
-    } catch (error) {
-      res.status(500).send({ error: 'Error scraping the webpage.' });
-    }
-  });
-  
-  // ... (rest of the code)
-  
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
