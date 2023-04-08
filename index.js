994c0b2a28c6f57840a9213b394ee53dab3fff6c
javron <towersjavin@gmail.com>
2023-04-08 15:21:13 -0700

    Fix Vercel deployment

--- index.js
+++ index.js
@@ -5,11 +5,18 @@ const cheerio = require('cheerio');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const path = require('path');
+const fs = require('fs');
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // ... (previous imports and setup)
-
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+  });
+  
 app.post('/scrape', async (req, res) => {
     const url = req.body.url;
     if (!url) {
